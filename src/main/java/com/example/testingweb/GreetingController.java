df3577eba717f716e26c27f3db4250841c7a52d9
timliu <84539606@qq.com>
2021-03-05 15:08:00 +0800

    添加了add api

--- src/main/java/com/example/testingweb/GreetingController.java
+++ src/main/java/com/example/testingweb/GreetingController.java
@@ -1,11 +1,13 @@
 package com.example.testingweb;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class GreetingController {
 
 	private final GreetingService service;
@@ -13,6 +15,13 @@ public class GreetingController {
 	public GreetingController(GreetingService service) {
 		this.service = service;
 	}
+
+
+	@GetMapping("/add")
+	public  Integer add(Integer a,Integer b){
+		return a+b;
+	}
+
 
 	@RequestMapping("/greeting")
 	public @ResponseBody String greeting() {
